5535315cb8e7add389f50a784aa5bca514840703
luishpm <luishpm@gmail.com>
2023-10-10 14:28:48 -0600

    Adds logs

--- src/app/api/validate-max-items/route.ts
+++ src/app/api/validate-max-items/route.ts
@@ -2,9 +2,12 @@ import {NextRequest, NextResponse} from "next/server";
 
 export async function POST(request: NextRequest) {
     let cart = await request.json()
+    console.log(cart)
     let itemsTotal = cart.lineItems.reduce((acc:any, curr:any) => {
         return acc + curr.quantity;
     }, 0);
+
+    console.log(`itemsTotal->${itemsTotal}`)
 
     if (itemsTotal <= 1) {
         return new NextResponse(null, {status: 200});
