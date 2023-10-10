5610f49c5f05bc429010b646619483dc89445c4f
luishpm <luishpm@gmail.com>
2023-10-10 12:21:46 -0600

    Returns an empty response

--- src/app/api/print-body/route.ts
+++ src/app/api/print-body/route.ts
@@ -1,12 +1,11 @@
 import {NextRequest, NextResponse} from "next/server";
 
 export async function POST(request: NextRequest) {
-    console.log(request.body);
-    return NextResponse.json({ success: true }, { status: 200 })
+    console.log(await request.json());
+    return new NextResponse(null, {status: 200})
 }
 
 export async function GET() {
     console.log("Probando");
     return NextResponse.json({ success: true }, { status: 200 })
-
 }
